3c3d2667c49133370a16bedb8a43518212739cb1
rafavrech <rafavrech@gmail.com>
2020-06-14 21:52:40 -0300

    algumas mudancas2

--- src/main/java/com/vrechbot/strategy/PoundStrategy.java
+++ src/main/java/com/vrechbot/strategy/PoundStrategy.java
@@ -1,9 +1,15 @@
 package com.vrechbot.strategy;
 
+import com.vrechbot.repository.PriceActionRepository;
+import com.vrechbot.service.MessageService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PoundStrategy extends PairStrategy {
+
+    public PoundStrategy(PriceActionRepository priceActionRepository, MessageService messageService) {
+        super(priceActionRepository, messageService);
+    }
 
     @Override
     public String getType() {
